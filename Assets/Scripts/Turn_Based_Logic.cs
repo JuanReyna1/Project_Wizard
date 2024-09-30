9e1549ad1e1b9cf4d61fa39f4811e9ff6f7667e3
Juan D.Reyna Pacheco <oo.jr54@gmail.com>
2024-09-29 20:15:59 -0600

    Completed Core Gameplay mechanics
    Implemented the core gameplay mechanics or attack and turn waits. We can add more mechanics easily in the future, such as block and scrolls.

--- Assets/Scripts/Turn_Based_Logic.cs
+++ Assets/Scripts/Turn_Based_Logic.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using TMPro;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public enum Battle_State { Start, PlayerTurn, EnemyTurn, Win, Lost }
 
@@ -13,22 +16,37 @@ public class Turn_Based_Logic : MonoBehaviour
 
     public Battle_State currentState;
 
+    [Header ("Prefabs & Scripts")]
     public GameObject playerPreFab;
     public GameObject[] enemiesPreFab;
     public GameObject enemyPreFab;
-
     public Transform playerPos;
     public Transform enemyPos;
+    public player_Weapons playerW;
+
+    [Header ("Hud")]
+    public Canvas playerHUD;
+    public Slider playerHealth;
+    public Slider enemyHealth;
+    public GameObject txtDisplay;
+    private GameObject bckGround;
+    private TMP_Text display;
+    public Button att1;
+    public Button att2;
+    public Button block;
+    public Button spAtt;
+    public GameObject sysEv;
+
     private Unit_Class playerUnit;
     private Unit_Class enemyUnit;
 
-    public player_Weapons playerW;
+    readonly System.Random rng = new System.Random();
 
-    System.Random rng = new System.Random();
-
-    private void Start()
+    void Start()
     {
-        
+
+        bckGround = txtDisplay.transform.parent.gameObject;
+        display = txtDisplay.GetComponent<TMP_Text>();
         currentState = Battle_State.Start;
         StartCoroutine(startBattle());
 
@@ -41,6 +59,8 @@ public class Turn_Based_Logic : MonoBehaviour
         playerUnit = player.GetComponent<Unit_Class>();
         playerUnit.setName("Wizard");
         playerUnit.setHealth(100);
+        playerW.setWeapon(0, 10, "Warlock Punch");
+        playerW.setWeapon(1, 15, "Spiked Wand");
 
         GameObject enemyGO = Instantiate(enemyPreFab, enemyPos);
         enemyUnit = enemyGO.GetComponent<Unit_Class>();
@@ -50,10 +70,26 @@ public class Turn_Based_Logic : MonoBehaviour
         enemyUnit.setWeapon(0, 15, "Attack 2");
 
         //Test display here
+        bckGround.SetActive(true);
+        display.text = "Starting battle...";
 
         //Set Hud player and enemy stats
+        playerHealth.maxValue = playerUnit.getHealth();
+        playerHealth.value = playerUnit.getHealth();
+
+        enemyHealth.maxValue = enemyUnit.getHealth();
+        enemyHealth.value = enemyUnit.getHealth();
 
         yield return new WaitForSeconds(2f);
+
+        //display
+        display.text = "Begins in 3 seconds!";
+
+        yield return new WaitForSeconds(3f);
+
+        //Display
+        bckGround.SetActive(false);
+        display.text = "";
 
         currentState = Battle_State.PlayerTurn;
         playerTurn();
@@ -66,6 +102,7 @@ public class Turn_Based_Logic : MonoBehaviour
         yield return new WaitForSeconds(2f);
 
         enemyUnit.takeDmg(playerW.getWeapon(weaponSlot).getDamage());
+        enemyHealth.value = enemyUnit.getHealth();
 
         yield return new WaitForSeconds(2f);
 
@@ -77,11 +114,18 @@ public class Turn_Based_Logic : MonoBehaviour
     IEnumerator enemyTurn()
     {
 
+        //currentState = Battle_State.EnemyTurn;
+
+        //Display
+        bckGround.SetActive(true);
+        display.text = "Enemy Turn";
+
         yield return new WaitForSeconds(3f);
 
-        int attackSlot = rng.Next(1, 2);
+        int attackSlot = rng.Next(0, 1);
 
         playerUnit.takeDmg(enemyUnit.getWeapons(attackSlot).getDamage());
+        playerHealth.value = playerUnit.getHealth();
 
         if (playerUnit.getHealth() <= 0)
         {
@@ -101,27 +145,32 @@ public class Turn_Based_Logic : MonoBehaviour
 
         //Let player know it is their turn
         Debug.Log("Your Turn.");
+        
+        //Display
+        bckGround.SetActive(true);
+        display.text = "Player Turn";
+
+        sysEv.SetActive(true);
 
     }
 
-    public void onAttackButton1()
+    public void onAction(int attack)
     {
+
+        sysEv.SetActive(false);
 
         if (currentState == Battle_State.PlayerTurn)
         {
 
-            StartCoroutine(attackEnemy(1));
+            bckGround.SetActive(false);
+            display.text = "";
 
-        }
-    }
+            if (attack <= 1)
+            {
 
-    public void onAttackButton2()
-    {
+                StartCoroutine(attackEnemy(attack));
 
-        if (currentState == Battle_State.PlayerTurn)
-        {
-
-            StartCoroutine(attackEnemy(2));
+            }
 
         }
     }
